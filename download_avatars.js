02403102dbaf31e7e1a6ccd96220e65aa3bee333
tahuana <tahuana@gmail.com>
2017-06-05 21:11:16 +0000

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -6,21 +6,19 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
   var options = {
     url: requestURL,
     headers: { 'User-Agent': 'GitHub Avatar Downloader - Student Project' }
   };
 
-  request(options, function(error, responde, body) {
-    // var json = JSON.parse(body);
-
-    console.log(body);
-
+  request(options, function(error, response, body) {
+    var json = JSON.parse(body);
+    cb(error, json);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  for (var i = 0; i < result.length; i++) {
+      console.log(result[i].avatar_url);
+  }
 });
