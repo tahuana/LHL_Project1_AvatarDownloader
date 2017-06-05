0c07fe07a416fadd0786b17e1cf0153438c2f0ea
tahuana <tahuana@gmail.com>
2017-06-05 20:01:46 +0000

    created the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,13 @@
 var request = require('request');
+var GITHUB_USER = "tahuana";
+var GITHUB_TOKEN = "08e8cb7da8586800c80a4e22648500b94268d63b";
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
